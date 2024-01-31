2a18ab77a08c9dbe6fb4a911f38d527f1b505435
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-01-31 15:58:28 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Init');
+const express = require('express');
+
+const app = express();
+const PORT = 5555;
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+})
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
