9b1d70bdb0541f8dbce2b446d02f80dd92b4a2a3
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-01 22:35:18 +0200

    Add about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
     res.render('home', {title: "Catalog Page"})
 });
 
+router.get('/about', (req, res) => {
+    res.render('about', {title: "About Page"})
+});
+
 module.exports = router;
