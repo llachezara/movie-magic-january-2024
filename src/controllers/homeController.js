b34b98ac45a5228baf64baabe7af205b163ee8e5
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-03 23:34:26 +0200

    Add search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,6 +10,13 @@ router.get('/about', (req, res) => {
     res.render('about', {title: "About Page"})
 });
 
+router.get('/search', (req, res) => {
+    const {title, genre, year} = req.query;
+    const movies = movieService.search(title, genre, year);
+
+    res.render('search', {title: "Search Page", movies})
+});
+
 router.get('/404', (req, res) =>{
     res.render('404')
 });
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -31,6 +31,22 @@ const movies = [
     }
 ];
 
+exports.search = function (title, genre, year){
+   let result = movies.slice();
+
+   if (title) {
+     result = result.filter(movie => movie.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()) );
+   }
+   if (genre) {
+    result = result.filter(movie => movie.genre.toLocaleLowerCase() === genre.toLocaleLowerCase() );
+   }
+   if (year) {
+    result = result.filter(movie => movie.year === year );
+   }
+
+   return result;
+}
+
 exports.create = function (movieData) {
     movieData._id = movies[movies.length-1]._id + 1;
     movies.push(movieData)
