4b6de2c15cf57e98c9955ac730f4e295013df1bc
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-03 11:50:53 +0200

    Add body-parser
    Take data from form in create view

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
+
 router.get('/movie/create', (req, res) => {
     res.render('create', {title: "Movie Create Page"})
+})
+
+router.post('/movie/create', (req, res) => {
+    console.log(req.body);
+    res.end();
 })
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const configExpress = require('./config/configExpress.js')
 const configHandlebars= require('./config/configHandlebars.js')
+const bodyParser = require('body-parser');
+
 const routes = require('./routes.js');
 
 const app = express();
 const PORT = 5555;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 configExpress(app);
 configHandlebars(app);
 
