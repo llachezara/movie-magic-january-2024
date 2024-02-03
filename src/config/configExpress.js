1321bb22ea870212af67b4cf1fa114bf3957a220
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-03 12:26:45 +0200

    Replace bodyParser with express urlencoded

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 function configExpress(app){
     app.use(express.static(path.join(__dirname, '../static')));
+    app.use(express.urlencoded({extended: false}));
 
     return app;
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 
 const configExpress = require('./config/configExpress.js')
 const configHandlebars= require('./config/configHandlebars.js')
-const bodyParser = require('body-parser');
 
 const routes = require('./routes.js');
 
 const app = express();
 const PORT = 5555;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 configExpress(app);
 configHandlebars(app);
 
