016f639f721973c2ff6df5a854a28eba5139dc85
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-06 16:05:37 +0200

    Create cast model

---
+++ src/models/Cast.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const castSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        required:true,
+        min: 14,
+        max: 120
+    },
+    born: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    nameInMovie: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    imageUrl:{
+        type: String,
+        required: true,
+        trim: true,
+        match: /^https?\/\//
+    },
+
+})
+
+module.exports = mongoose.model('Cast', castSchema);
