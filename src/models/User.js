105310bfc1a8157a188fdbb2c8c9eeec55e07d60
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-08 21:24:56 +0200

    Add rePassword virtual setter for password confirmation

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { MongooseError } = require('mongoose/lib/mongoose');
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -8,10 +10,21 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true,
+        minLength: 8
     }
 });
 
-userSchema.pre('svae', function (params) {
-    
+userSchema.virtual('rePassword')
+    .set(function(value) {
+        
+        if (value !== this.password) {
+             throw new mongoose.MongooseError('Password mismatch!')
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
 })
+
 module.exports = mongoose.model('User', userSchema);
