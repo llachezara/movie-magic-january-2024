697a1d57dde5c73186aea6b94de7add81e5c7ee9
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-11 21:17:19 +0200

    Implement validation for Movie and Cast schema

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -4,23 +4,29 @@ const castSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        minLength: [5, "Actor's name should be at least 5 characters long!"],
+        match: /[A-Za-z0-9\s]+/
     },
     age: {
         type: Number,
         required:true,
-        min: 14,
+        min: 1,
         max: 120
     },
     born: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        minLength: [10, "Birth place should be at least 10 characters long!"],
+        match: /[A-Za-z0-9\s]+/
     },
     nameInMovie: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        minLength: [5, "Character's name should be at least 5 characters long!"],
+        match: /[A-Za-z0-9\s]+/
     },
     imageUrl:{
         type: String,
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -4,24 +4,29 @@ const movieSchema = new mongoose.Schema({
     title: { 
         type: String, 
         required: true,
-        trim: true 
+        trim: true,
+        minLength: [5, 'Title should be at least 5 characters long!'],
+        match: /[A-Za-z0-9\s]+/
     },
     genre: { 
         type: String, 
         required: true,
         trim: true,
-        match: /^[A-Z]/
+        minLength: [5, 'Genre should be at least 5 characters long!'],
+        match: /[A-Za-z0-9\s]+/
     },
     director: { 
         type: String, 
         required: true,
-        trim: true
+        trim: true,
+        minLength: [5, 'Director name should be at least 5 characters long!'],
+        match: /[A-Za-z0-9\s]+/
     },
     year: {
         type: Number,
         required: true,
         min: 1900,
-        max: 2030
+        max: 2024
     },
     imageUrl: {
         type: String,
@@ -39,7 +44,8 @@ const movieSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        maxLength: 1000
+        minLength: [20, 'Description should be at least 20 characters long!'],
+        match: /[A-Za-z0-9\s]+/
     },
     casts: [{
         type: mongoose.Schema.Types.ObjectId,
