4d632d9f2fdc4fc9ffae97931b5565955d5f3e60
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-11 22:51:30 +0200

    Add error handling for create cast

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const castService = require('../services/castService');
 const {isAuth} = require('../middlewares/authMiddleware');
+const {getErrorMessage} = require('../utils/errorUtils');
 
 router.get('/create', isAuth, (req, res) =>{
     res.render('cast/create', {title: "Create Cast"});
@@ -8,11 +9,13 @@ router.get('/create', isAuth, (req, res) =>{
 
 router.post('/create', async (req, res) =>{
     const cast = req.body;
+
     try{
         await castService.create(cast);
         res.redirect('/');
     }catch(err){
-        console.log(err);
+       const message = getErrorMessage(err);
+       res.render('cast/create', {title: "Create Cast", ...cast, error: message})
     }
 
 })
