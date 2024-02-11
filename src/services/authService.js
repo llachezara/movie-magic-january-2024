0510dea6e937d2d2d1270c17b6ae757e9bd2e6de
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-02-11 22:40:36 +0200

    Handle register when user already exists

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,8 +3,21 @@ const bcrypt = require('bcrypt');
 const jwt = require('../lib/jwt');
 const { SECRET } = require('../config/config');
 
-exports.register = function (userData){
+exports.register = async function (userData){
+
+    const user = await User.findOne({ email: userData.email });
     
+    if (user) {
+        throw new Error('Email already exists!');
+    }
+    // try {
+    //     await User.create(userData);
+    // } catch (err) {
+    //     if (err.code === 11000) {
+    //         throw new Error('Email already exists!');
+    //     }
+    // }
+
     return User.create(userData);
 };
 
