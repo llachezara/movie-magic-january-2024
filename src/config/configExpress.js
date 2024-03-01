c11dbbdc1fde3e140fdbc5841eef9a078faebd30
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-03-01 22:52:02 +0200

    Update configExpress.js

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const {auth} = require('../middlewares/authMiddleware');
 
 function configExpress(app){
-    app.use(express.static(path.join(__dirname, '../static')));
+    app.use(express.static(path.resolve('src/static')));
     app.use(express.urlencoded({extended: false}));
     app.use(cookieParser());
     app.use(auth);
